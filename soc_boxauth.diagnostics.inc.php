1480a2eaf62be2ebac6031b14ada13f7b706f52d
richard <r.chardgoodrow@gmail.com>
2015-11-21 15:11:40 -0600

    diag file

---
+++ soc_boxauth.diagnostics.inc.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once("PHPBox/BoxFolder.php");
+
+function _soc_boxauth_diagnostics() {
+
+	dpm("curl https://api.box.com/oauth2/token \
+-d 'grant_type=refresh_token&refresh_token={$_SESSION['box']['refresh_token']}&client_id=" 
+. variable_get(SOC_BOXAUTH_CLIENTID_VARIABLE) . "&client_secret=" . variable_get(SOC_BOXAUTH_CLIENTSECRET_VARIABLE) . " ' \
+-X POST");
+
+
+  $content = '<pre>';
+  $content .= print_r($_SESSION['box'], true);
+
+	
+	
+	$folder = New BoxFolder(0, $_SESSION['box']['access_token']);
+	
+
+
+	$content .= print_r($folder->getItems(), true);
+
+
+
+	$content .='</pre>';
+	return $content;
+
+}
