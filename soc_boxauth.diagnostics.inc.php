38455ba33378e1d7ffdf1fff83efd110ae5d2982
rag227 <rgoodrow@northwestern.edu>
2015-11-30 12:04:14 -0600

    Diag page and /box/stop/session item

--- soc_boxauth.diagnostics.inc.php
+++ soc_boxauth.diagnostics.inc.php
@@ -4,26 +4,19 @@ require_once("PHPBox/BoxFolder.php");
 
 function _soc_boxauth_diagnostics() {
 
-	dpm("curl https://api.box.com/oauth2/token \
--d 'grant_type=refresh_token&refresh_token={$_SESSION['box']['refresh_token']}&client_id=" 
-. variable_get(SOC_BOXAUTH_CLIENTID_VARIABLE) . "&client_secret=" . variable_get(SOC_BOXAUTH_CLIENTSECRET_VARIABLE) . " ' \
+    // Dump what I think the refresh token call should be
+    dpm("curl https://api.box.com/oauth2/token \
+-d 'grant_type=refresh_token&refresh_token={$_SESSION['box']['refresh_token']}&client_id="
+    . variable_get(SOC_BOXAUTH_CLIENTID_VARIABLE) . "&client_secret=" . variable_get(SOC_BOXAUTH_CLIENTSECRET_VARIABLE) . " ' \
 -X POST");
 
 
-  $content = '<pre>';
-  $content .= print_r($_SESSION['box'], true);
+    $content = '<pre>';
+    $content .= print_r($_SESSION['box'], true);
 
-	
-	
-	$folder = New BoxFolder(0, $_SESSION['box']['access_token']);
-	
+    $folder = New BoxFolder(0, $_SESSION['box']['access_token']);
+    $content .= print_r($folder->getItems(), true);
 
-
-	$content .= print_r($folder->getItems(), true);
-
-
-
-	$content .='</pre>';
-	return $content;
-
+    $content .='</pre>';
+    return $content;
 }
