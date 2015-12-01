ff785643d360b29471129a8685bc3886801363f8
rag227 <rgoodrow@northwestern.edu>
2015-12-01 11:25:55 -0600

    Got renew token to FINALLY WORK!!

--- PHPBox/BoxFolderOperations.class.php
+++ PHPBox/BoxFolderOperations.class.php
@@ -28,15 +28,26 @@ class BoxFolderOperations {
 
 
   public static function getCurrentAccessToken() {
+
+    // If we have an access token session in memeory return it.
     if (isset($_SESSION['box'])) {
-      return $_SESSION['box']['access_token'];
+      if (isset($_SESSION['box']['access_token'])) {
+        return $_SESSION['box']['access_token'];
+      }
+      else {
+        dpm($_SESSION);
+        return false;
+      }
     }
+
+
     else {
       watchdog('Box Operations', 'Session not started');
-      return drupal_set_message('Box session not active', 'error');
+      drupal_set_message('Box session not active', 'error');
+      return FALSE;
+
     }
   }
-
 
 
   /**
@@ -94,13 +105,45 @@ class BoxFolderOperations {
   }
 
 
-  public static function doPost($url, $postdata, $header) {
+  public static function doPost($url, $postdata, $header, $dataencode = 'JSON') {
+
+
+    // Catch bad encoding by issuing an error. The array $encode_type will
+    // contain acceptable methods. Return false and stop processing here.
+    $encode_types = ['QUERY', 'HTTP', 'JSON'];
+    if (!in_array($dataencode, $encode_types)) {
+      $msg = t('ERROR in call to @function in @file. Data Enocde must be !list', [
+        '@function' => __FUNCTION__,
+        '@file' => __FILE__,
+        '!list' => implode(', ', $encode_types),
+      ]);
+      watchdog('Box Operations', $msg);
+      drupal_set_message($msg, 'error');
+
+      return FALSE;
+    }
+
+    // Decide how to encode data. Some calls will require post data to be
+    // encoded as a JSON string. Other calls will require QUERY.
+    switch ($dataencode) {
+
+      case 'QUERY':
+      case 'HTTP':
+        $postdata = http_build_query($postdata);
+        break;
+      case 'JSON':
+      default:
+        $postdata = trim(json_encode($postdata));
+        break;
+    }
+
+
     // cURL magic
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
     curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($postdata));
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
     curl_setopt($ch, CURLOPT_HTTPHEADER, array(
       $header,
     ));
--- soc_boxauth.diagnostics.inc.php
+++ soc_boxauth.diagnostics.inc.php
@@ -4,12 +4,6 @@
 
 function _soc_boxauth_diagnostics() {
 
-    // Dump what I think the refresh token call should be
-    dpm("curl https://api.box.com/oauth2/token \
--d 'grant_type=refresh_token&refresh_token={$_SESSION['box']['refresh_token']}&client_id="
-    . variable_get(SOC_BOXAUTH_CLIENTID_VARIABLE) . "&client_secret=" . variable_get(SOC_BOXAUTH_CLIENTSECRET_VARIABLE) . " ' \
--X POST");
-
 
     $content = '<pre>';
     $content .= print_r($_SESSION['box'], true);
